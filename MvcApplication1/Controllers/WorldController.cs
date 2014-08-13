50860d1d90de6a87976fc0b89240d1ec40916421
Mentioum <mentioummetis@gmail.com>
2014-08-13 21:44:06 +0100

    Added world model and had world controller return it.

---
+++ MvcApplication1/Controllers/WorldController.cs
@@ -0,0 +1,32 @@
+﻿using MvcApplication1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MvcApplication1.Controllers
+{
+    public class WorldController : ApiController
+    {
+
+        public World[] Get()
+        {
+            return new World[]
+            {
+                new World
+                {
+                    Id = 1,
+                    Name = "The First World"
+                },
+                new World
+                {
+                    Id = 2,
+                    Name = "The Second World"
+                }
+            };
+        }
+
+    }
+}
---
+++ MvcApplication1/Models/World.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcApplication1.Models
+{
+    public class World
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+
+    }
+}
